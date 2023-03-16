82529c6fc0d738806b7eb6858909c5b0648e45a9
Jay Kang <65272953+jykang96@users.noreply.github.com>
2023-03-16 17:24:05 -0400

    popular fitness page made

---
+++ IOS-Project/IOS-Project/ViewControllers/PopularFitnessRoutineViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PopularFitnessRoutineViewController.swift
+//  IOS-Project
+//
+//  Created by Jay on 2023-03-16.
+//
+
+import UIKit
+
+class PopularFitnessRoutineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
